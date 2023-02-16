b7edccec335002b29982c9a5334622a392c54627
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-02-16 18:39:34 +0800

    change test name of basic test

--- react-testing/src/components/Basic/Index.test.tsx
+++ react-testing/src/components/Basic/Index.test.tsx
@@ -4,5 +4,6 @@ import Person from "./Index";
 test('describe person (Basic Component Test)', () => {
     render(<Person name="sofyan" />)
     const divPerson = screen.getByRole('person');
+    
     expect(divPerson).toHaveTextContent('sofyan');
 });

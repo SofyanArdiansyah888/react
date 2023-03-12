8cd33e31b4ede0ce3a1ff86f777373e2822a3161
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-03-13 06:09:07 +0800

    mui testing

---
+++ react-mui/src/components/Buttons.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import {PhotoCamera,AccessTimeTwoTone} from '@mui/icons-material';
+const Buttons = () => {
+  return (
+    <div style={{display: 'flex', flexDirection:'column', maxWidth: "200px"}}>
+      <Button
+        variant="contained"
+        color="secondary" 
+        disableElevation
+        size="medium"
+        onClick={() => alert("this is default button")}
+      >
+        Button Default
+      </Button>
+      <IconButton color="primary" size="small" aria-label="Upload Button" component="label">
+        <input type="file" hidden />
+        <PhotoCamera /> 
+      </IconButton>
+      <Button variant="outlined" color="success" endIcon={<AccessTimeTwoTone  />}>
+            Hello
+      </Button>
+    </div>
+  );
+};
+
+export { Buttons };
